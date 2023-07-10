0299b0cdba7b0fde2c007b20095e912f7a7bb84e
Mirco Rudolph <mirco.rudolph@gmail.com>
2023-07-10 21:41:07 +0700

    refinded try and except block for more logical flow and precision of errors

--- api_call_openai.py
+++ api_call_openai.py
@@ -4,7 +4,7 @@ import openai
 API_KEY = "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"
 openai.api_key  = API_KEY
 
-def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0):
+def get_completion_from_messages(messages, temperature, model="gpt-3.5-turbo"):
     response = openai.ChatCompletion.create(
         model=model,
         messages=messages,
@@ -19,7 +19,7 @@ def conversation(messages, role = "system", content = "Start the conversation.",
         # Append new user or system message to message stack
         messages.append({"role": role, "content": content})
         
-        response = get_completion_from_messages(messages, temperature=temperature)
+        response = get_completion_from_messages(messages, temperature)
 
         role = response.choices[0].message.role
         content = response.choices[0].message.content
@@ -27,13 +27,11 @@ def conversation(messages, role = "system", content = "Start the conversation.",
         # Append new answer of assistant to message stack
         messages.append({"role": role, "content": content})
         
-        return(messages, "Assistent", content)
+        return(messages, role.capitalize(), content)
 
     except Exception as error:
         # Remove the last message from the message stack if an error has been thrown by openai API
         # If the error was thrown by openai API, the last message on the stack is the user or system message
         messages.pop()
-        return(
-            messages,
-            "System",
-            "Your request was ok. But there was an error down the line. Please try again")
+        # reraise an exception for passing it further down
+        raise
--- application.py
+++ application.py
@@ -28,50 +28,69 @@ def delivery_handler_v1():
 
 def handler(get_context):
 
-    # Get request data
-    role = request.form.get('role')
-    content = request.form.get('content')
-    language = request.form.get('language')
+    supported_roles = ['user']
+    supported_languages = ['English', 'German', 'French', 'Spanish']
 
-    # First call, session is empty
-    if not session:
-        print("First call")
-        # There is no existing session.
-        # Set messages to the context
-        # Call the passed function to receive context in given language
-        messages =  get_context(language)
+    try:
 
-    # Consecutive call, session is not empty
-    else:
-        print("Consecutive call")
-        # There is an existing session.
-        # Retrieve the messages from that session
-        messages = session.get("messages")
+        # Get request data
+        role = request.form.get('role')
+        content = request.form.get('content')
+        language = request.form.get('language')
+
+        # Check if the payload is correct
+
+        # Empty content is not allowed
+        if not content:
+            response = {"content": "Error: Invalid request. Content is empty."}
+            return response, 400
+
+        # In order to protect from setting different system messages,
+        # only certain roles are allowed
+        if role not in supported_roles:
+            response = {"content": "Error: Invalid request. Not supported role. Only 'user' is allowed."}
+            return response, 400
+
+        # Limit the possible languages
+        if language not in supported_languages:
+            response = {"content": "Error: Invalid request. Not supported language. Only 'English', 'German', 'French', 'Spanish' is allowed."}
+            return response, 400
+
+        # First call, session is empty
+        if not session:
+            print("First call")
+            # There is no existing session.
+            # Set messages to the context
+            # Call the passed function to receive context in given language
+            messages =  get_context(language)
+
+        # Consecutive call, session is not empty
+        else:
+            print("Consecutive call")
+            # There is an existing session.
+            # Retrieve the messages from that session
+            messages = session.get("messages")
+
+        # insert into try catch block
+        # Process the request asynchronously using ThreadPoolExecutor
+        with ThreadPoolExecutor() as executor:
+            future = executor.submit(apicall, messages, role, content)
+            messages, role, content = future.result()
+
+        # Save the new messages array in session object
+        session["messages"] = messages
+
+        response = {"content": f"{role}: {content}"}
+        
+        return response, 200
+
+    # Catch errors and return error code
+    except Exception as error:
+        print(error)
+        return "Sorry! Something went wrong.", 500
 
 
-    # In order to protect from setting different system messages,
-    # only requests with role 'user' are allowed
-    if role not in ['user'] or not content:
-        response = {"content": "Website: Invalid request. Either the content was empty or the role was not 'user'"}
-        return response, 400
-
-    # insert into try catch block
-    # Process the request asynchronously using ThreadPoolExecutor
-    with ThreadPoolExecutor() as executor:
-        future = executor.submit(apicall, messages, role, content, language)
-        messages, role, content = future.result()
-
-    # Save the new messages array in session object
-    session["messages"] = messages
-
-    response = {"content": f"{role}: {content}"}
-    
-    return response, 200
-    
-    # catch
-    # return 400
-
-def apicall(messages, role, content, language):
+def apicall(messages, role, content):
 
     # Process the request based on role and content
     messages, role, content = conversation(messages, role, content)
